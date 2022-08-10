63286f77cf6fcaf2543b2e82ce4aaac1c26ba37a
Tyler Beddow <TylerLeeBeddow@hotmail.co.uk>
2022-08-10 17:09:18 +0100

    Add project files.

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,87 @@
+﻿namespace ConsoleApp3
+{
+    internal class Program
+    {
+        public class Stopwatch
+        {
+            private readonly DateTime StartTime;
+            private DateTime EndTime { get; set; }
+            private bool IsRunning = false;
+            public TimeSpan Duration { get; set; }
+
+            public Stopwatch(DateTime startTime)
+            {
+                this.StartTime = startTime;
+            }
+
+            public void Start()
+            {
+                if(IsRunning == false)
+                {
+                    IsRunning = true;
+                    Console.WriteLine("Stopwatch started");
+                }
+                else
+                {
+                    throw new InvalidOperationException("Cannot start stopwatch as it is already running");
+                }
+            }
+
+            public void Stop()
+            {
+                if (IsRunning == false)
+                {
+                    throw new InvalidOperationException("Cannot stop stopwatch as it is not running");
+                }
+                else
+                {
+                    EndTime = DateTime.UtcNow;
+
+                    Console.WriteLine($"Stopwatch stopped");
+
+                    Duration = EndTime - StartTime;
+
+                    IsRunning = false;
+                }
+            }
+        }
+        static void Main(string[] args)
+        {
+            Stopwatch watch = new Stopwatch(DateTime.UtcNow);
+
+            try
+            {
+                watch.Start();
+                Thread.Sleep(5000);
+                watch.Stop();
+                Console.WriteLine($"Stopwatch was running for total duration of {watch.Duration}");
+            }
+            catch (InvalidOperationException)
+            {
+                throw;
+            }
+
+            try
+            {
+                watch.Start();
+                Thread.Sleep(3000);
+                watch.Stop();
+                Console.WriteLine($"Stopwatch was running for total duration of {watch.Duration}");
+            }
+            catch (InvalidOperationException)
+            {
+                throw;
+            }
+
+            try
+            {
+                watch.Start();
+                watch.Start();
+            }
+            catch (InvalidOperationException)
+            {
+                throw;
+            }
+        }
+    }
+}
